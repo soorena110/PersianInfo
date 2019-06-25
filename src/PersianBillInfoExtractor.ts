60cc3fd0dc4ae941bb41b5a416e8f32c9a8691ca
azarang <azarangmr@gmail.com>
2019-06-25 11:36:23 +0430

    Add Control bit checking

--- src/PersianBillInfoExtractor.ts
+++ src/PersianBillInfoExtractor.ts
@@ -9,15 +9,19 @@ const billServiceTypes = {
     "8": "جرایم راهنمایی و رانندگی"
 } as any;
 
-export default class PersianBillInfoExtractor {
+
+export default class PersianBill {
     /**
      * returns information as {billPrice, yearCode, period}.
      * will return undefined if length of paymentId is less than 13
      */
-    static getPaymentInfo(paymentId: string | number) {
+    static getPaymentInfo(billId: string | number, paymentId: string | number) {
         if (typeof paymentId !== 'string')
             paymentId = paymentId.toString();
-        if (paymentId.length < 13)
+        if (typeof billId !== 'string')
+            billId = billId.toString();
+
+        if (!isValidPaymentId(paymentId, billId))
             return undefined;
 
         const billPrice = parseInt(paymentId.substring(0, paymentId.length - 5)) * 1000;
@@ -33,15 +37,61 @@ export default class PersianBillInfoExtractor {
     static getBillInfo(billId: string | number) {
         if (typeof billId !== 'string')
             billId = billId.toString();
-        if (billId.length < 13)
+        if (!isValidBillId(billId))
             return undefined;
 
         const serviceTypeCode = billId[billId.length - 2];
         const serviceType = billServiceTypes[serviceTypeCode];
-        const serviceImage =  `https://raw.githubusercontent.com/soorena110/PersianInfo/master/images/${serviceTypeCode}.png`;
+        const serviceImage = `https://raw.githubusercontent.com/soorena110/PersianInfo/master/images/${serviceTypeCode}.png`;
         const placeCode = billId.substr(billId.length - 5, 3);
         const subscribeNumber = billId.substr(0, billId.length - 5);
 
         return {serviceType, placeCode, subscribeNumber, serviceImage}
     }
 }
+
+const isValidPaymentId = (paymentId: string, billId: string) => {
+    if (paymentId.length < 6 || paymentId.length > 13 || billId.length < 6 || billId.length > 13)
+        return false;
+
+    const checksum = compute11CheckSum(paymentId.substr(0, paymentId.length - 2));
+    if (checksum != paymentId[paymentId.length - 2])
+        return false;
+
+    const paymentControlBit = computePaymentControlBit(paymentId.substr(0, paymentId.length - 1), billId);
+    return paymentControlBit == paymentId[paymentId.length - 1];
+
+};
+
+const computePaymentControlBit = (paymentIdWithoutLastBit: string, billId: string) => {
+    const removeRightZeros = (id: string) => {
+        while (id[id.length - 1] == '0')
+            id = id.substr(0, id.length - 1);
+        return id;
+    };
+
+    const mergedPaymentIdAndBillIdWithoutRightZeros = removeRightZeros(billId) + removeRightZeros(paymentIdWithoutLastBit);
+    return compute11CheckSum(mergedPaymentIdAndBillIdWithoutRightZeros);
+};
+
+
+const isValidBillId = (billId: string) => {
+    if (billId.length < 6 || billId.length > 13)
+        return false;
+
+    const checksum = compute11CheckSum(billId.substr(0, billId.length - 1));
+    return checksum == billId[billId.length - 1];
+};
+const compute11CheckSum = (billId: string) => {
+    let sum = 0;
+    for (let i = 0; i < billId.length; i++) {
+        const coefficient = i % 6 + 2;
+        const ithDigitFromLast = billId[billId.length - i - 1];
+        sum += coefficient * Number(ithDigitFromLast);
+    }
+
+    const remainingOn11 = sum % 11;
+    if (remainingOn11 == 0 || remainingOn11 == 1)
+        return '0';
+    return (11 - remainingOn11).toString();
+};
--- src/_dev/index.ts
+++ src/_dev/index.ts
@@ -1,5 +1,23 @@
-import PersianBillInfoExtractor from "../PersianBillInfoExtractor";
+import PersianBill from "../PersianBillInfoExtractor";
 
-(window as any).PersianBillInfoExtractor = PersianBillInfoExtractor;
+console.warn('Bill ↓');
+console.log(PersianBill.getBillInfo(8608748200140));
+console.log(PersianBill.getBillInfo(1677036253));
+console.log(PersianBill.getBillInfo(1677036252));
 
-console.log(PersianBillInfoExtractor.getPaymentInfo(1234567890123));
+console.warn('Payment ↓');
+console.log('8608748200140, 24050400', PersianBill.getPaymentInfo(8608748200140, 24050400));
+console.log('8608748200140, 24050420', PersianBill.getPaymentInfo(8608748200140, 24050420));
+console.log('8608748200140, 24050403', PersianBill.getPaymentInfo(8608748200140, 24050403));
+console.log('8608748200141, 24050400', PersianBill.getPaymentInfo(8608748200141, 24050400));
+console.log('8608748200130, 24050400', PersianBill.getPaymentInfo(8608748200130, 24050400));
+
+
+console.warn('Payment ↓');
+console.log('772263913142, 25100068', PersianBill.getPaymentInfo(772263913142, 25100068));
+console.log('772263913142, 25100067', PersianBill.getPaymentInfo(772263913142, 25100067));
+console.log('772263913142, 25100078', PersianBill.getPaymentInfo(772263913142, 25100078));
+console.log('772263913143, 25100068', PersianBill.getPaymentInfo(772263913143, 25100068));
+console.log('772263913132, 25100068', PersianBill.getPaymentInfo(772263913132, 25100068));
+console.log('772263913124, 25100068', PersianBill.getPaymentInfo(772263913124, 25100068));
+console.log('772263913124, 25100067', PersianBill.getPaymentInfo(772263913124, 25100067));
